4b161de410a2af41d4e7ec3994dcb14624955444
David D <dsdiazs@falabella.cl>
2021-04-04 18:48:32 -0400

    modify path of build and comment favicon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/resume-angular2021'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/resume-angular2021/index.html'));
 });
