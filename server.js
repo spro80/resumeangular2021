d9534361335377edce65b33cdbfb31137adea562
David D <dsdiazs@falabella.cl>
2021-04-04 19:26:19 -0400

    add port listen

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ app.use(express.static(__dirname+'/dist/resume-angular2021'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/resume-angular2021/index.html'));
 });
+
+
+app.set('port', process.env.PORT || 3000 )
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("[server.js] App is running at localhost:" + app.get('port'));
+});
+
+module.exports = app
